41f86ed2c2fdae6a4f820bb44cd054793937e550
Trevor Fehrman <trevorfehrman@gmail.com>
2021-01-10 14:57:05 -0800

    pulling in data

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,8 +1,18 @@
 import * as React from 'react'
+import 'firebase/firestore'
+
 import { Switch, Route } from 'react-router-dom'
+import { useFirestore, useFirestoreCollectionData, useUser } from 'reactfire'
+
 import { MyComponent } from 'components/MyComponent'
 
 const AuthenticatedApp: React.FC = () => {
+  const user = useUser()
+
+  const testCollectionRef = useFirestore().collection('test')
+  const testCollectionDocuments = useFirestoreCollectionData(testCollectionRef, { idField: 'docId' })
+  console.log({ testCollectionDocuments, user })
+
   return (
     <Switch>
       <Route path="/">
