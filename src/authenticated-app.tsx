3a5352ecb7cd3e0dee5a5330e7214af900f3dd72
Trevor Fehrman <trevorfehrman@gmail.com>
2021-01-10 15:23:14 -0800

    react-hook-form and chakra input with form control integrated

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -3,8 +3,13 @@ import 'firebase/firestore'
 
 import { Switch, Route } from 'react-router-dom'
 import { useFirestore, useFirestoreCollectionData, useUser } from 'reactfire'
+import { useForm } from 'react-hook-form'
 
-import { MyComponent } from 'components/MyComponent'
+import { FormErrorMessage, FormLabel, FormControl, Input, Button } from '@chakra-ui/react'
+
+type FormData = {
+  name: string
+}
 
 const AuthenticatedApp: React.FC = () => {
   const user = useUser()
@@ -13,10 +18,34 @@ const AuthenticatedApp: React.FC = () => {
   const testCollectionDocuments = useFirestoreCollectionData(testCollectionRef, { idField: 'docId' })
   console.log({ testCollectionDocuments, user })
 
+  const { handleSubmit, errors, register, formState } = useForm<FormData>()
+
+  function onSubmit(values: FormData) {
+    console.log(values)
+  }
+
+  function validateName(value: string) {
+    console.log(value)
+    if (value === 'Steve') {
+      return 'Fuck you, Steve'
+    } else {
+      return true
+    }
+  }
+
   return (
     <Switch>
       <Route path="/">
-        <MyComponent myProp="hi" />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FormControl isInvalid={!!errors.name}>
+            <FormLabel htmlFor="name">Name</FormLabel>
+            <Input name="name" placeholder="name" ref={register({ validate: validateName })} />
+            <FormErrorMessage>{errors.name && errors.name.message}</FormErrorMessage>
+          </FormControl>
+          <Button mt={4} colorScheme="teal" isLoading={formState.isSubmitting} type="submit">
+            Submit
+          </Button>
+        </form>
       </Route>
     </Switch>
   )
