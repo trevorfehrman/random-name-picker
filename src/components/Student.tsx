0e88f4312792f816de86d423796a61bfa02a2ede
Steve Lorentzen <stevelorentzen@Steves-iMac.local>
2021-01-26 20:18:02 -0800

    used Flex component from chakra instead of normal div with flex property

--- src/components/Student.tsx
+++ src/components/Student.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import styled from '@emotion/styled'
-import { Heading, IconButton } from '@chakra-ui/react'
+import { Heading, IconButton, Flex } from '@chakra-ui/react'
 import { DeleteIcon } from '@chakra-ui/icons'
 import { useFirestore, useUser } from 'reactfire'
 
@@ -9,13 +9,13 @@ type StudentProps = {
   studentInStudentGroupId: string
 }
 
-const StudentContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  border: 1px solid black;
-  padding: 10px;
-`
+// const StudentContainer = styled.div`
+//   display: flex;
+//   justify-content: space-between;
+//   align-items: center;
+//   border: 1px solid black;
+//   padding: 10px;
+// `
 
 const Student: React.FC<StudentProps> = ({ studentName, studentInStudentGroupId }) => {
   const { data: user } = useUser()
@@ -31,12 +31,12 @@ const Student: React.FC<StudentProps> = ({ studentName, studentInStudentGroupId 
   }
 
   return (
-    <StudentContainer>
+    <Flex align="center" justify="space-between" padding="10px" border="1px solid black">
       <Heading as="h3" size="md">
         {studentName}
       </Heading>
       <IconButton icon={<DeleteIcon />} aria-label="delete" onClick={removeHandler} />
-    </StudentContainer>
+    </Flex>
   )
 }
 
