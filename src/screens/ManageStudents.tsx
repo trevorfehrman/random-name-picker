768930ef6421e894fd584137e17113c2a4e46bbe
DoctorBurglar <65787113+DoctorBurglar@users.noreply.github.com>
2021-02-17 22:19:29 -0800

    created a ManageStudents component that shows all of a teachers students

--- src/screens/ManageStudents.tsx
+++ src/screens/ManageStudents.tsx
@@ -1,10 +1,23 @@
 import * as React from 'react'
 import { Heading } from '@chakra-ui/react'
+import { useFirestore, useFirestoreCollectionData, useUser } from 'reactfire'
+import { IStudent } from 'interfacesAndTypes/interfacesAndTypes'
 
 const ManageStudents: React.FC = () => {
+  const { data: user } = useUser()
+
+  const teacherRef = useFirestore().collection('teachers').doc(user.uid)
+  const allStudentsRef = teacherRef.collection('students')
+  const allStudentsDocuments = useFirestoreCollectionData<IStudent & { docId: string }>(allStudentsRef, {
+    idField: 'docId',
+  }).data
+
   return (
     <>
       <Heading as="h1">All Students</Heading>
+      {allStudentsDocuments?.map(doc => {
+        return <div key={doc.docId}>{doc.studentName}</div>
+      })}
     </>
   )
 }
