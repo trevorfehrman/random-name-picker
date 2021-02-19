f816198d3c19f5065a814e9eec449a412c55775d
DoctorBurglar <65787113+DoctorBurglar@users.noreply.github.com>
2021-02-19 13:32:33 -0800

    Switched to a mobile first design Adjusted styles in several components to look better on mobile

---
+++ src/styles.tsx
@@ -0,0 +1,26 @@
+import styled from '@emotion/styled'
+
+export const StudentContainer = styled.div`
+  display: flex;
+  align: center;
+  justify-content: space-between;
+  padding: 10px;
+  border: 1px solid black;
+  border-radius: 5px;
+  width: 100%;
+  margin-top: 3px;
+`
+
+export const FormBox = styled.div`
+  w: 90%;
+  max-width: 90%;
+  margin: 0 0 0 0;
+  padding: 10px;
+`
+
+export const PageContentsBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`
