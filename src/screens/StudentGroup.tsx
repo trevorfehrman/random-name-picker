eb98339d063ad8c1ef22e59febe33b0e971ae826
Steve Lorentzen <stevelorentzen@gmail.com>
2021-03-06 21:03:38 -0800

    refactor StudentGroup component made a custom component for the Editable component and associated functions/state

--- src/screens/StudentGroup.tsx
+++ src/screens/StudentGroup.tsx
@@ -4,22 +4,14 @@ import { useParams, useHistory } from 'react-router-dom'
 import 'firebase/firestore'
 import { useFirestore, useUser, useFirestoreDocData, useFirestoreCollectionData } from 'reactfire'
 import {
-  Button,
   Flex,
   useDisclosure,
-  Icon,
-  IconButton,
-  Input,
   FormControl,
   FormErrorMessage,
-  FormLabel,
-  Box,
   Editable,
   EditablePreview,
   EditableInput,
-  outline,
 } from '@chakra-ui/react'
-import { BiExpand } from 'react-icons/bi'
 import { IStudentGroup, IStudent, IStudentInStudentGroup, Params } from 'interfacesAndTypes'
 import FullScreenDisplay from 'components/FullScreenDisplay'
 import AddExistingStudentsModal from 'components/AddExisitingStudentsModal'
@@ -37,14 +29,6 @@ type FormData = {
   studentGroup: string
 }
 
-const StudentGroupNameForm = styled.form`
-  width: 16rem;
-  max-width: 90%;
-  text-align: right;
-  display: flex;
-  justify-content: flex-end;
-`
-
 const StudentGroup: React.FC = () => {
   const { isOpen, onOpen, onClose } = useDisclosure()
 
@@ -52,18 +36,6 @@ const StudentGroup: React.FC = () => {
   const [selectedStudent, setSelectedStudent] = React.useState<IStudentInStudentGroup | null>(null)
   const [fullScreenDisplayIsOpen, setFullScreenDisplayIsOpen] = React.useState(false)
   const [isTimeToShuffle, setIsTimeToShuffle] = React.useState(false)
-  const [errors, setErrors] = React.useState<string[]>([])
-
-  // const { handleSubmit, errors, register, formState } = useForm()
-
-  // function validateName(value: string) {
-  //   console.log('this is running')
-  //   if (!value) {
-  //     return 'Name is required'
-  //   } else if (value !== 'Naruto') {
-  //     return "Jeez! You're not a fan 😱"
-  //   } else true
-  // }
 
   const history = useHistory()
   const params: Params = useParams()
@@ -146,115 +118,11 @@ const StudentGroup: React.FC = () => {
     history.push(`/show-all-students/${studentGroupId}`)
   }
 
-  const editStudentGroupNameHandler = (value: string) => {
-    console.log(value)
-    // if (values.length > 22) {
-    //   console.log('hello?')
-    //   return
-    // }
-    studentGroupRef.update({ studentGroupName: value }).catch(err => {
-      console.log(err)
-    })
-  }
-
-  const checkForErrors = (value: string) => {
-    const errorArray = []
-    if (value === '') {
-      errorArray.push('Please enter a group name')
-    }
-    if (value.length >= 35) {
-      errorArray.push('Please enter a value with less than 35 characters')
-    }
-    setErrors(errorArray)
-    if (errorArray.length > 0) {
-      return true
-    } else {
-      return false
-    }
-  }
-
-  const mySubmitHandler = (value: string) => {
-    const thereAreErrors = checkForErrors(value)
-    if (!thereAreErrors) {
-      editStudentGroupNameHandler(value)
-    }
-  }
-
   return (
     <PageContentsBox>
       <HeadingBoxWithBackButton backHandler={backHandler}>
-        {/* <EditableStudentGroupName studentGroupRef={studentGroupRef} studentGroupDocument={studentGroupDocument} /> */}
         <Flex justify="flex-end">
-          <StudentGroupNameForm>
-            <FormControl isInvalid={errors.length > 0} display="flex" flexDirection="column" alignItems="flex-end">
-              {/* {studentGroupDocument && (
-                <Input
-                  defaultValue={studentGroupDocument.studentGroupName}
-                  fontSize="1.2rem"
-                  fontWeight="bolder"
-                  w="100%"
-                  minWidth="200px"
-                  textAlign="right"
-                  color="blue.900"
-                  // onBlur={handleSubmit(editStudentGroupNameHandler)}
-                  // onSubmit={mySubmitHandler}
-                  name="studentGroup"
-                  ref={register({ maxLength: 22, required: true })}
-                />
-              )}
-              <FormErrorMessage>
-                {errors.studentGroup?.type === 'maxLength' && <p>Please pick a name with 22 characters or less</p>}
-              </FormErrorMessage>
-              <FormErrorMessage>
-                {errors.studentGroup?.type === 'required' && <p>Please put something</p>}
-              </FormErrorMessage> */}
-              {studentGroupDocument && (
-                <Editable
-                  defaultValue={studentGroupDocument.studentGroupName}
-                  placeholder="Student Group Name"
-                  w="100%"
-                  textAlign="right"
-                  color="blue.900"
-                  fontSize="1.2rem"
-                  fontWeight="bolder"
-                  // onBlur={handleSubmit(editStudentGroupNameHandler)}
-                  onSubmit={mySubmitHandler}
-                >
-                  <EditablePreview _hover={{ cursor: 'pointer' }} />
-                  <EditableInput />
-                  {/* ref={register({ maxLength: 22, required: true })}  */}
-                </Editable>
-                // <Input
-                //   name="studentGroupName"
-                //   variant="unstyled"
-                //   defaultValue={studentGroupDocument.studentGroupName}
-                //   maxWidth="90%"
-                //   textAlign="right"
-                //   border="none"
-                //   fontSize="1.3rem"
-                //   fontWeight="bold"
-                //   color="blue.900"
-                //   _after={{ backgroundColor: 'white' }}
-                //   _hover={{ border: '1px solid black' }}
-                //   ref={register({ maxLength: 22, required: true })}
-                //   onBlur={handleSubmit(editStudentGroupNameHandler)}
-                // />
-              )}
-              {errors.map(errorMessage => {
-                return (
-                  <FormErrorMessage margin={0} key={errorMessage}>
-                    {errorMessage}
-                  </FormErrorMessage>
-                )
-              })}
-              {/* <FormErrorMessage>
-                {errors.studentGroupName?.type === 'maxLength' && <p>Please pick a name with 22 characters or less</p>}
-              </FormErrorMessage>
-              <FormErrorMessage>
-                {errors.studentGroupName?.type === 'required' && <p>Please put something</p>}
-              </FormErrorMessage> */}
-            </FormControl>
-          </StudentGroupNameForm>
+          <EditableStudentGroupName studentGroupDocument={studentGroupDocument} studentGroupRef={studentGroupRef} />
         </Flex>
       </HeadingBoxWithBackButton>
       <NewStudent
@@ -270,13 +138,6 @@ const StudentGroup: React.FC = () => {
         setFullScreenDisplayIsOpen={setFullScreenDisplayIsOpen}
         selectHandler={selectHandler}
       />
-
-      {/* <Flex flexWrap="wrap" justifyContent="space-evenly" width="100%" padding="1rem">
-        <Button onClick={showAllHandler}>Show All</Button>
-        <Button colorScheme="blue" onClick={selectHandler}>
-          Select Name
-        </Button>
-      </Flex> */}
 
       <UnselectedStudents unselected={unselected} studentGroupId={studentGroupId} />
 
