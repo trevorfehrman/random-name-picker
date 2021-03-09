3a3d1dd814d0067acb359f080c875e1e7c420a48
Steve Lorentzen <stevelorentzen@gmail.com>
2021-03-08 22:15:54 -0800

    Created a fade effect to give visual cue that only part of the list is being displayed Also used a Chevron for the same purpose

--- src/components/StudentList.tsx
+++ src/components/StudentList.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
-import { Heading, Flex, Button, Box } from '@chakra-ui/react'
+import { Heading, Flex, Button, Box, Icon } from '@chakra-ui/react'
+import { ChevronUpIcon, ChevronDownIcon } from '@chakra-ui/icons'
 import { IStudentInStudentGroup } from 'interfacesAndTypes'
 import StudentInGroup from 'components/StudentInGroup'
 import styled from '@emotion/styled'
@@ -16,10 +17,14 @@ const StudentList: React.FC<StudentListProps> = ({ studentGroupId, studentsInThi
 
   return (
     <Box width="100%" textAlign="center">
-      <Button margin="0 auto" w="10rem" marginBottom=".5rem" onClick={() => setIsShowingAll(!isShowingAll)}>
-        {isShowingAll ? 'Show Some' : 'Show All'}
+      <Button w="10rem" marginBottom=".5rem" onClick={() => setIsShowingAll(!isShowingAll)}>
+        <Flex justify="space-between" w="100%">
+          {isShowingAll ? <ChevronUpIcon /> : <ChevronDownIcon />}
+          {isShowingAll ? <span>Show Some</span> : <span>Show All</span>}
+          {isShowingAll ? <ChevronUpIcon /> : <ChevronDownIcon />}
+        </Flex>
       </Button>
-      <Box width="100%" maxHeight={isShowingAll ? '' : '16.21rem'} overflow="hidden">
+      <Box width="100%" maxHeight={isShowingAll ? '' : '16.21rem'} overflow="hidden" position="relative">
         <AnimateSharedLayout>
           {studentsInThisStudentGroup?.map(doc => {
             console.log(studentsInThisStudentGroup)
@@ -33,6 +38,48 @@ const StudentList: React.FC<StudentListProps> = ({ studentGroupId, studentsInThi
             )
           })}
         </AnimateSharedLayout>
+
+        {!isShowingAll && (
+          <Box
+            backgroundImage="linear-gradient(to bottom, rgba(255, 255, 255, 0), rgba(255, 255, 255, 1))"
+            w="100%"
+            height="8.5rem"
+            z-index={3}
+            position="absolute"
+            bottom={0}
+          ></Box>
+        )}
+        {isShowingAll ? (
+          <ChevronUpIcon
+            position="absolute"
+            bottom={0}
+            fontSize="3rem"
+            marginLeft="auto"
+            marginRight="auto"
+            left={0}
+            right={0}
+            textAlign="center"
+            onClick={() => setIsShowingAll(!isShowingAll)}
+            _hover={{
+              cursor: 'pointer',
+            }}
+          />
+        ) : (
+          <ChevronDownIcon
+            position="absolute"
+            bottom={0}
+            fontSize="3rem"
+            marginLeft="auto"
+            marginRight="auto"
+            left={0}
+            right={0}
+            textAlign="center"
+            onClick={() => setIsShowingAll(!isShowingAll)}
+            _hover={{
+              cursor: 'pointer',
+            }}
+          />
+        )}
       </Box>
     </Box>
   )
