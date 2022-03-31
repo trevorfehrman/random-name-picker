40366809aa099f805fd6872f1d78b41d825a3090
Steve Lorentzen <Stevelorentzen@gmail.com>
2022-03-31 11:11:38 -0700

    created desktop header with nav fixed bug in studentGroup component

--- src/components/MobileHeader.tsx
+++ src/components/MobileHeader.tsx
@@ -1,46 +1,46 @@
 import * as React from 'react'
 import { NavLink } from 'react-router-dom'
-import styled from '@emotion/styled'
 import { Tabs, TabList, Tab } from '@chakra-ui/react'
 import { useLocation } from 'react-router'
 
-const TabStyled = styled(Tab)`
-  width: 100%;
-  height: 100%;
-  color: var(--main-color-medium);
-  text-transform: uppercase;
-  font-size: 1.1rem;
-  font-weight: 400;
-`
+const tabStyles = {
+  width: '100%',
+  height: '100%',
+  color: 'var(--main-color-medium)',
+  fontSize: '1.1rem',
+  fontWeight: '400',
+}
 
-const Header: React.FC = () => {
+const MobileHeader: React.FC = () => {
   const location = useLocation()
 
   return (
     <>
-      <Tabs color="var(--main-color-medium)" variant="unstyled">
+      <Tabs color="var(--main-color-medium)" variant="unstyled" display={['', '', 'none']}>
         <TabList>
           <NavLink to="/" exact style={{ width: '50%', height: '4rem' }}>
-            <TabStyled
+            <Tab
               borderBottom={
                 location.pathname === '/' ? '2px solid var(--main-color-medium)' : '2px solid var(--grey-medium)'
               }
+              {...tabStyles}
             >
               Groups
-            </TabStyled>
+            </Tab>
           </NavLink>
 
           <NavLink to="/manage-students" style={{ width: '50%', height: '4rem' }}>
-            <TabStyled
+            <Tab
               borderLeft="1px solid var(--grey-medium)"
               borderBottom={
                 location.pathname === '/manage-students'
                   ? '2px solid var(--main-color-medium)'
                   : '2px solid var(--grey-medium)'
               }
+              {...tabStyles}
             >
               Students
-            </TabStyled>
+            </Tab>
           </NavLink>
         </TabList>
       </Tabs>
@@ -48,4 +48,4 @@ const Header: React.FC = () => {
   )
 }
 
-export default Header
+export default MobileHeader
