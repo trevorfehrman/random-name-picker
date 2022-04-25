c420832447bcef5e8a135b74a00cc3be2df7369a
Steve Lorentzen <stevelorentzen@gmail.com>
2022-04-24 19:17:06 -0700

    switched to creating teacher docs in the sign in function instead of in cloud functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,20 +1,20 @@
-import * as functions from 'firebase-functions'
+// import * as functions from 'firebase-functions'
 
-import * as admin from 'firebase-admin'
-admin.initializeApp()
+// import * as admin from 'firebase-admin'
+// admin.initializeApp()
 
-exports.createUserDoc = functions.auth.user().onCreate(user => {
-  console.log(user)
-  admin
-    .firestore()
-    .collection('teachers')
-    .doc(user.uid)
-    .set({
-      email: user.email,
-      displayName: user.displayName,
-      photoURL: user.photoURL,
-      uid: user.uid,
-    })
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
-})
+// exports.createUserDoc = functions.auth.user().onCreate(user => {
+//   console.log(user)
+//   admin
+//     .firestore()
+//     .collection('teachers')
+//     .doc(user.uid)
+//     .set({
+//       email: user.email,
+//       displayName: user.displayName,
+//       photoURL: user.photoURL,
+//       uid: user.uid,
+//     })
+//     .then(res => console.log(res))
+//     .catch(err => console.log(err))
+// })
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,20 +1,92 @@
 import * as React from 'react'
-import { useAuth } from 'reactfire'
-import { StyledFirebaseAuth } from 'react-firebaseui'
+import { useFirestore } from 'reactfire'
+import firebase from 'firebase'
+import { Button } from '@chakra-ui/button'
+
+const createTeacherCode = (length: number) => {
+  const charArray = []
+  let classroomCode = ''
+  for (let i = 48; i <= 57; i++) {
+    charArray.push(String.fromCharCode(i))
+  }
+  for (let i = 65; i <= 90; i++) {
+    charArray.push(String.fromCharCode(i))
+  }
+  for (let i = 1; i <= length; i++) {
+    classroomCode += charArray[Math.floor(Math.random() * charArray.length)]
+  }
+  return classroomCode
+}
 
 const Login: React.FC = () => {
-  const auth = useAuth
+  console.log('login page')
 
-  const uiConfig = {
-    signInFlow: 'popup',
-    signInOptions: [auth.GoogleAuthProvider.PROVIDER_ID],
-    callbacks: {
-      // Avoid redirects after sign-in.
-      signInSuccessWithAuthResult: () => false,
-    },
+  const auth = firebase.auth()
+
+  const google = new firebase.auth.GoogleAuthProvider()
+
+  const teacherCollectionRef = useFirestore().collection('teachers')
+
+  function handleSignIn() {
+    auth
+      .signInWithPopup(google)
+      .then(result => {
+        if (!result.user) {
+          return
+        }
+        console.log('user', result.user)
+        const { displayName, email, uid, photoURL } = result.user
+        console.log(result)
+        return teacherCollectionRef.doc(uid).set({
+          displayName,
+          email,
+          photoURL,
+          teacherCode: createTeacherCode(6),
+          uid,
+        })
+      })
+      .then(result => {
+        console.log('ooooh', result)
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }
 
-  return <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth()} />
+  // const uiConfig = {
+  //   signInFlow: 'popup',
+  //   signInOptions: [auth.GoogleAuthProvider.PROVIDER_ID],
+  //   callbacks: {
+  //     // Returning false avoids redirects after sign-in.
+  //     // signInSuccess: (result: any) => {
+  //     //   console.log('this ran')
+  //     // },
+  //     signInSuccessWithAuthResult: (result: any) => {
+  //       const { displayName, email, uid, photoURL } = result.data
+  //       console.log('on login')
+  //       teacherCollectionRef
+  //         .doc(uid)
+  //         .set({
+  //           displayName,
+  //           email,
+  //           photoURL,
+  //           teacherCode: createTeacherCode(6),
+  //           uid,
+  //         })
+  //         .then(result => {
+  //           console.log(result)
+  //         })
+  //         .catch(err => {
+  //           console.log(err)
+  //         })
+  //       return false
+  //     },
+  //   },
+  // }
+
+  // return <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth()} />
+
+  return <Button onClick={handleSignIn}>SignIn</Button>
 }
 
 export { Login }
